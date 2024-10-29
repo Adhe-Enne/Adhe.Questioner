8692ade666c0a3f48ff252938cd5067eeef6032c
Adhe-Enne <ninadaniel_service@hotmail.com>
2024-10-28 22:11:51 -0300

    feat: Se Crea proyecto tipo Api

---
+++ Questioner.Api/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Questioner.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        // GET: DefaultController
+        [HttpGet]
+        public string Get()
+        {
+            return "Application Running... ";
+        }
+
+
+    }
+}
---
+++ Questioner.Domain/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Questioner.Domain.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(20)")]
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
