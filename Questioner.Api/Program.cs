59c417e8ba8386c1fc622bfc4d6ee4f99da07af5
Daniel Nina <ninadaniel_service@hotmail.com>
2025-09-30 01:27:53 -0300

    feat: Inyeccion exitosa
    Se da limpieza de referencias y proyectos sin utilidad

--- Questioner.Api/Program.cs
+++ Questioner.Api/Program.cs
@@ -1,3 +1,8 @@
+using Core.Framework.StartUp;
+using Questioner.Data;
+using Questioner.Repository;
+using Questioner.Business;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +10,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerWithJwt("Questioner.Api", "v1");
+
+//inyectar dependencias
+builder.Services.AddMySqlContext(builder.Configuration);
+builder.Services.AddRepository();
+builder.Services.AddServicesBusiness();
+
+builder.Services.AddAuthConfig(builder.Configuration);
+
+var allowedOrigin = builder.Configuration.GetValue<string>("AllowedOrigin") ?? "AllowedOrigin";
+var withOrigins = builder.Configuration.GetSection("WithOrigins").Get<string[]>() ?? Array.Empty<string>();
+
+builder.Services.AddCorsConfiguration(allowedOrigin, withOrigins);
 
 var app = builder.Build();
+
+app.UseCors(allowedOrigin);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
